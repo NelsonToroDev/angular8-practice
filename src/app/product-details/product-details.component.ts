c92802535e25bf2b556cd776bbe81c954109c2ee
Nekero8 <nekero8@gmail.com>
2019-07-01 22:55:55 -0400

    Managing Data

--- src/app/product-details/product-details.component.ts
+++ src/app/product-details/product-details.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 
 import { products } from '../products'; // import from products.ts file
+import { CartService } from '../cart.service';
 
 @Component({
   selector: 'app-product-details',
@@ -12,12 +13,18 @@ export class ProductDetailsComponent implements OnInit {
   product;
 
   constructor(
-    private route: ActivatedRoute
+    private route: ActivatedRoute,
+    private cartService: CartService
   ) { }
 
   ngOnInit() {
     this.route.paramMap.subscribe(params => {
       this.product = products[+params.get('productId')];
     })
+  }
+
+  addProductToCart(product){
+    window.alert('Your product has been added to the cart!');
+    this.cartService.addToCart(product);
   }
 }
